f482c05676087c8ba9d876e258d08b6e665b3d9d
abdobassel <abdobassel@gmail.com>
2024-04-04 12:53:57 +0200

    re

--- lib/features/sqlite/presentation/pages/test.dart
+++ lib/features/sqlite/presentation/pages/test.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 //import 'package:flutter_application_1/features/sqlite/data/models/person.dart';
 import 'package:flutter_application_1/features/sqlite/presentation/cubit/sqlite_cubit.dart';
-import 'package:flutter_application_1/features/sqlite/presentation/pages/person_model.dart';
+import 'package:flutter_application_1/features/sqlite/data/models/person_model.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 //import 'package:country_icons/country_icons.dart';
 
